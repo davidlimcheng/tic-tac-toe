355c90a2f1981d19f0f6a8ac022cb80545d4f781
David Lim Cheng <davidlimcheng@gmail.com>
2019-04-07 10:16:10 -0700

    documentation, cleaning up comments in code, minor refactoring

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-# tic-tac-toe
+# Tic-Tac-Toe
+This is my implementation of the game tic-tac-toe with the following requirements:
+- Has a user interface
+- Enforces the basic rules, and determines the outcome of the game
+- Ability to start the game
+- Ability to take turns with a computer player to play the game
+- Ability to evaluate the winner
+- Ability to stop an ongoing game at any point
+- Proper messaging in appropriate state changes
+
+## Demo & Installation
+The game is currently hosted using Github Pages and is accessible from https://davidlimcheng.github.io/tic-tac-toe/.
+
+To install and run the game on a local development environment:
+- Clone the repository
+- Install dependencies
+- Run the `server` script defined in `package.json`:
+```
+npm install
+npm run server
+```
+The application will be available on `localhost:8080`.
+
+## Overview
+For the technical implementation, my priority was to develop an application that was as lean as possible, which is why I chose to go with HTML and Javascript. The implementation mainly relies on using ES6 modules, DOM traversal, and classes for state management.
+
+As for computer difficulty, I aimed to strike a balance between a computer that was challenging, but still beatable the majority of the time. To do this, I prioritized computer moves in the following order:
+1. Winning
+2. Not losing
+3. Any available space
+
+In other words, the computer should play to win if it sees any openings. It should play defense if it sees any "near-losses". If neither scenarios exist, it will randomly pick any available space.
+
+## Technical Details
+
+### DOM Management
+The application relies on native DOM traversal functions to replace HTML based on user actions. It uses `eventListeners` at each space on the game board to react to user inputs, checking for end-game scenarios, occupying a space if none exist. 
+
+There are two views: `mainView` and `gameView`. These are both managed in the entrypoint, `application.js`, which switches between the two based on user input.
+
+### State Management
+Game state is encapsulated in the `Game` class. This class also holds all logic for computer strategy.
+
+`eventListeners` are placed on each game space, using the `handlePlayerTurn` function as a callback. Each player turn updates the state, which is a collection of class instance properties.
+
+The most important property is `gameSpaceMap`, which manages the state of the game board. It is an object consisting of mappings between the game space ID and its value, with `1` signifying a player-held space and `-1` signifying a computer-held space.
+```
+{
+  'top-l': 0,
+  'top-c': 0,
+  'top-r': 1,
+  'mid-l': 0, 
+  'mid-c': 0, 
+  'mid-r': 0, 
+  'bot-l': -1, 
+  'bot-c': 0, 
+  'bot-r': 0
+}
+```
+Would map to a game board with the state of:
+```
+_|_|X 
+_|_|_
+O| |
+```
+Other instance properties include booleans for messaging management, and additional booleans along with a `totalTurns` counter to allow for finer control of logic flow.
+
+#### Computer Logic
+Knowing that a player space is occupied by a `1` and a computer space is occupied by a `-1`, we can deduce "hot-spots", or any spots that would be crucial to winning or losing by creating an array of each "line" and its values, and summing over them. 
+
+For example, take the following board:
+```
+X|_|X 
+_|_|_
+ |O|
+```
+The top "line" would be defined as `['top-l', 'top-c', 'top-r']`. Transforming this to an array with the actual player values, we'd get `[1, 0, 1]`. Reducing this by sum gives us a value of `2`, which we know is a "near-loss" spot, allowing us to prioritize the importance of playing that spot for the next turn.
+
+The same idea applies to "near-win" spots, we would just look for any summations with a value of `-2`.
+
+We can also use this same algorithm to check for actual winners, looking instead for `3` or `-3` values.
+
+## Opportunities For Improvement
+
+- Accessibility
+- Error handling
+- Responsiveness
+- Testing
+- Making game board size configurable
+  - Programmatically build game space IDs
+  - Would allow for difference size game board on the fly
+- Making "win" amount configurable
+  - Ex. on a 5x5 board we can allow 4 consecutive spaces to win instead of 5
+- Computer difficulty
+  - Can make computer more difficult by leveraging a tree structure to search through possible moves
+  - Can also incorporate an opening move dictionary on larger game boards
--- src/javascript/Game.js
+++ src/javascript/Game.js
@@ -10,176 +10,11 @@ export default class Game {
     this.totalTurns = 0;
   }
 
-  /**
-   * @desc Randomly chooses and returns the ID of any available space on the game board.
-   * 
-   * Convert gameSpaceMap to array of ['top-l', 1] subarrays using Object.entries
-   * Ex. [['top-l', 1], ['top-c', 0], [...]]
-   * Filter out any subarray that does not have a 0 value
-   * Randomly choose one of the remaining ids and return it
-   */
-  anyAvailableSpaceId() {
-    const available = Object.entries(this.gameSpaceMap).reduce((availableSpaces, [id, spaceValue]) => {
-      return spaceValue == 0 ? availableSpaces.concat(id) : availableSpaces;
-    }, []);
-    // choose a random integer between 0 and length of array - 1
-    const max = available.length - 1;
-    const randomIndex = Math.floor(Math.random() * (max + 1));
-    return available[randomIndex];
-  }
-
-  /**
-   * @desc Finds the best available space.  Prioritize in the following order:
-   * 1. Winning - any row with sum of -2
-   * 2. Not Losing - any row with sum of 2
-   * 3. Random Space
-   * 
-   * Find any lines with sum of -2
-   *  Return the id of any available space in any of those lines
-   * Find any lines with sum of 2
-   *  Return the id of any available space in any of those lines
-   * If neither of the above exist, call anyAvailableSpace()
-   * 
-   * Only start looking for wins after this.totalTurns > 4
-   */
-  bestAvailableSpaceId() {
-    let possibleLossIndex, possibleWinIndex = false;
-    const lineObject = getLines(this.gameSpaceMap);
-    const { rows, cols, diags } = lineObject
-    const sumObject = {rows: reduceLinesToSums(rows), cols: reduceLinesToSums(cols), diags: reduceLinesToSums(diags)};
-
-    // When there have been three turns, we only need to check for possibleLoss, not win.
-    if (this.totalTurns == 3) {
-      possibleLossIndex = this.possibleLoss(sumObject);
-    } else {
-      possibleWinIndex = this.possibleWin(sumObject);
-      possibleLossIndex = !possibleWinIndex ? this.possibleLoss(sumObject) : false;
-    }
-
-    const bestMove = possibleWinIndex || possibleLossIndex;
-    return bestMove ? this.getIdFromPossibleIndex(lineObject, bestMove) : this.anyAvailableSpaceId();
-  }
-
-  getIdFromPossibleIndex(lineObject, moveIndex) {
-    const [lineType, lineIndex] = moveIndex;
-    const line = lineObject[lineType][lineIndex];
-    for (const moveObject of line) {
-      const [id, spaceValue] = Object.entries(moveObject)[0];
-      if (spaceValue === 0) return id;
-    }
-  }
-
-  possibleLoss(lines) {
-    for (const [id, line] of Object.entries(lines)) {
-      const possibleLossIndex = line.indexOf(2);
-      if (possibleLossIndex !== -1) {
-        return [id, possibleLossIndex];
-      }
-    }
-    return false;
-  }
-
-  possibleWin(lines) {
-    for (const [id, line] of Object.entries(lines)) {
-      const possibleWinIndex = line.indexOf(-2);
-      if (possibleWinIndex !== -1) {
-        return [id, possibleWinIndex];
-      }
-    }
-    return false;
-  }
-
-  /**
-   * @desc  Handles a click by the user on any game space. Does nothing if it is not the player's turn.
-   */
   handlePlayerTurn(event) {
     if (this.currentTurn == 1) {
       const id = event.target.id;
       this.updateGameState(id);
     }
-  }
-
-  /**
-   * @desc Handles the logic for each computer turn. We don't want this to be too easy, so we will
-   * apply logic that plays basic defense. We also don't want it to be too difficult, so whenever there is no
-   * chance of losing, we'll just pick a random available space. 
-   * If there is a chance of winning though, we should choose the winning space.
-   * 
-   * Furthermore, we should only check for possible losses when this.totalTurns >= 3. That is when opponent will have two out,
-   * since they always go first.
-   * Can start checking for possible wins when this.totalTurns >= 6
-   * 
-   * Can start checking for actual wins/losses when this.totalTurns >= 5. This is when opponent will have 3 out.
-   *   Actually we want to check for win/loss in updateGameState()
-   * 
-   * If totalTurns == 9, that's a draw
-   */
-  takeComputerTurn() {
-    let id;
-    if (this.totalTurns < 3) {
-      id = this.anyAvailableSpaceId();
-    } else if (this.totalTurns < 9) {
-      id = this.bestAvailableSpaceId();
-    }
-    this.updateGameState(id);
-  }
-
-  checkForGameEnd() {
-    if (this.totalTurns < 5) {
-      return;
-    } else if (this.totalTurns < 9) {
-      const lines = getLines(this.gameSpaceMap);
-      const { rows, cols, diags } = lines;
-      const sumObject = { rows: reduceLinesToSums(rows), cols: reduceLinesToSums(cols), diags: reduceLinesToSums(diags) };
-      // check for any wins
-      const winningLineIndex = this.checkForWinner(sumObject);
-      if (winningLineIndex) this.endGameForWin(lines, winningLineIndex);
-    } else {
-      // draw game
-      this.endGameForDraw();
-    }
-  }
-
-  checkForWinner(sumLines) {
-    for (const [id, line] of Object.entries(sumLines)) {
-      let winningIndex = line.indexOf(3);
-      winningIndex = winningIndex === -1 ? line.indexOf(-3) : winningIndex;
-
-      if (winningIndex !== -1) {
-        return [id, winningIndex];
-      }
-    }
-    return false;
-  }
-
-  endGameForDraw() {
-    // Style whole board
-    gameSpaceIds.forEach(id => document.querySelector(`div.${id}`).classList.add('draw-game'));
-
-    // change the message
-    document.getElementById('message').innerHTML = 'Looks like a draw game. Try again!';
-
-    this.gameOver = true;
-  }
-
-  endGameForWin(lines, winningLineIndex) {
-    const [lineType, lineIndex] = winningLineIndex;
-    const winningLine = lines[lineType][lineIndex];
-    const winningLineIds = winningLine.map(lineObject => Object.keys(lineObject)[0]);
-
-    // Style the winning line
-    const endgameClass = this.currentTurn === 1 ? 'player-win' : 'computer-win';
-    winningLineIds.forEach((id) => {
-      document.querySelector(`div.${id}`).classList.add(endgameClass);
-    });
-
-    // Disables all buttons
-    gameSpaceIds.forEach(id => document.getElementById(id).disabled = true);
-
-    // Change the message
-    const message = this.currentTurn === 1 ? 'Great job! You did it.' : 'So close! Try again.';
-    document.getElementById('message').innerHTML = message;
-    this.gameOver = true;
   }
 
   updateGameState(id) {
@@ -202,5 +37,122 @@ export default class Game {
       this.currentTurn = this.currentTurn * -1;
       if (this.currentTurn == -1) this.takeComputerTurn();
     }
+  }
+
+  takeComputerTurn() {
+    // Before there are 3 turns, there are no possible "near-loss" or "near-win" scenarios
+    const id = this.totalTurns < 3 ? this.anyAvailableSpaceId() : this.bestAvailableSpaceId();
+    this.updateGameState(id);
+  }
+
+  anyAvailableSpaceId() {
+    const available = Object.entries(this.gameSpaceMap).reduce((availableSpaces, [id, spaceValue]) => {
+      return spaceValue == 0 ? availableSpaces.concat(id) : availableSpaces;
+    }, []);
+
+    const max = available.length - 1;
+    const randomIndex = Math.floor(Math.random() * (max + 1));
+    return available[randomIndex];
+  }
+
+  bestAvailableSpaceId() {
+    let possibleLossIndex, possibleWinIndex = false;
+    const lineObject = getLines(this.gameSpaceMap);
+    const { rows, cols, diags } = lineObject
+    const sumObject = {rows: reduceLinesToSums(rows), cols: reduceLinesToSums(cols), diags: reduceLinesToSums(diags)};
+
+    // When there have been only three turns, a win is not yet possible
+    if (this.totalTurns == 3) {
+      possibleLossIndex = this.possibleLoss(sumObject);
+    } else {
+      possibleWinIndex = this.possibleWin(sumObject);
+      possibleLossIndex = !possibleWinIndex ? this.possibleLoss(sumObject) : false;
+    }
+
+    const bestMove = possibleWinIndex || possibleLossIndex;
+    return bestMove ? this.getIdFromPossibleIndex(lineObject, bestMove) : this.anyAvailableSpaceId();
+  }
+
+  getIdFromPossibleIndex(lineObject, moveIndex) {
+    const [lineType, lineIndex] = moveIndex;
+    const line = lineObject[lineType][lineIndex];
+    for (const moveObject of line) {
+      const [id, spaceValue] = Object.entries(moveObject)[0];
+      if (spaceValue === 0) return id;
+    }
+  }
+
+  possibleLoss(lineSums) {
+    for (const [id, line] of Object.entries(lineSums)) {
+      const possibleLossIndex = line.indexOf(2);
+      if (possibleLossIndex !== -1) {
+        return [id, possibleLossIndex];
+      }
+    }
+    return false;
+  }
+
+  possibleWin(lineSums) {
+    for (const [id, line] of Object.entries(lineSums)) {
+      const possibleWinIndex = line.indexOf(-2);
+      if (possibleWinIndex !== -1) {
+        return [id, possibleWinIndex];
+      }
+    }
+    return false;
+  }
+
+  checkForGameEnd() {
+    // There are no possible wins with less than 5 total turns
+    if (this.totalTurns < 5) {
+      return;
+    } else if (this.totalTurns < 9) {
+      const lines = getLines(this.gameSpaceMap);
+      const { rows, cols, diags } = lines;
+      const sumObject = { rows: reduceLinesToSums(rows), cols: reduceLinesToSums(cols), diags: reduceLinesToSums(diags) };
+
+      const winningLineIndex = this.checkForWinner(sumObject);
+      if (winningLineIndex) this.endGameForWin(lines, winningLineIndex);
+    } else {
+      this.endGameForDraw();
+    }
+  }
+
+  checkForWinner(sumLines) {
+    for (const [id, line] of Object.entries(sumLines)) {
+      // Check for player wins
+      let winningIndex = line.indexOf(3);
+      // Check for computer wins if there are no player wins
+      winningIndex = winningIndex === -1 ? line.indexOf(-3) : winningIndex;
+
+      if (winningIndex !== -1) {
+        return [id, winningIndex];
+      }
+    }
+    return false;
+  }
+
+  endGameForDraw() {
+    gameSpaceIds.forEach(id => document.querySelector(`div.${id}`).classList.add('draw-game'));
+    document.getElementById('message').innerHTML = 'Looks like a draw game. Try again!';
+
+    this.gameOver = true;
+  }
+
+  endGameForWin(lines, winningLineIndex) {
+    const [lineType, lineIndex] = winningLineIndex;
+    const winningLine = lines[lineType][lineIndex];
+    const winningLineIds = winningLine.map(lineObject => Object.keys(lineObject)[0]);
+
+    const endgameClass = this.currentTurn === 1 ? 'player-win' : 'computer-win';
+    winningLineIds.forEach((id) => {
+      document.querySelector(`div.${id}`).classList.add(endgameClass);
+    });
+
+    gameSpaceIds.forEach(id => document.getElementById(id).disabled = true);
+
+    const message = this.currentTurn === 1 ? 'Great job! You did it.' : 'So close! Try again.';
+    document.getElementById('message').innerHTML = message;
+    this.gameOver = true;
   }
 };
